81cfd686a1d1ca8fb06a2a024e6395e961c61b53
jeromexlee <jeromexlee@berkeley.edu>
2017-04-24 10:43:03 -0700

    refact

--- suppotLineCalculator.py
+++ suppotLineCalculator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import time
 import quandl
 import math
 import sys
@@ -29,6 +30,39 @@ def printSupports(AH, NH, AL, HL):
 	print("AL（最低值即强支撑点）: " + str(round(AL,2)))
 	print("HL（次低值即弱支撑点）: " + str(round(HL,2)))
 
+def calculateConfidence(data):
+	count = 0
+	total = len(data.index)
+	for i in range(3, len(data.index)):
+		AH, NH, AL, HL = calculateSupports(float(data.iloc[i - 3].high), float(data.iloc[i - 3].low), float(data.iloc[i - 2].high), float(data.iloc[i - 2].low), float(data.iloc[i - 2].close))
+		if (data.iloc[i - 1].close >= AH):
+			if upOrDip(data.iloc[i].open, data.iloc[i].close):
+				count+=1
+		elif (data.iloc[i - 1].close <= AL):
+			if not upOrDip(data.iloc[i].open, data.iloc[i].close):
+				count+=1
+		else:
+			count+=1
+
+	print("The correctness for this stock is", round((count / total) * 100, 2), "%")
+
+def main():
+	start_time = time.time()
+	if len(sys.argv) > 3:
+		sys.argv = [float(sys.argv[i]) for i in range(1,6)]
+		AH, NH, AL, HL = calculateSupports(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+		printSupports(AH, NH, AL, HL)
+	else:
+		ticker = sys.argv[1]
+		quandl.ApiConfig.api_key = '6PcspJiyEshZTzxZYgHZ'
+		data = quandl.get_table('WIKI/PRICES', ticker = ticker.upper(), paginate=True)
+		if len(sys.argv) > 2 and sys.argv[2] == "T":
+			calculateConfidence(data)
+		AH, NH, AL, HL = calculateSupports(float(data.iloc[-2].high), float(data.iloc[-2].low), float(data.iloc[-1].high), float(data.iloc[-1].low), float(data.iloc[-1].close))
+		printSupports(AH, NH, AL, HL)
+	elapsed_time = time.time() - start_time
+	print("This process takes ", elapsed_time, "s")
+
 # [main funciton]
 # @params: 前日最高点， 前日最低点，昨日最高点，昨日最低点，昨日收盘价
 # @params: trickerName
@@ -38,29 +72,5 @@ def printSupports(AH, NH, AL, HL):
 # 运行方法2：python supporLineCalculator.py tricker name
 # example: python suppotLineCalculator.py AAPL
 if __name__ == '__main__':
-	if len(sys.argv) > 2:
-		sys.argv = [float(sys.argv[i]) for i in range(1,6)]
-		AH, NH, AL, HL = calculateSupports(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-		printSupports(AH, NH, AL, HL)
-	else:
-		ticker = sys.argv[1]
-		quandl.ApiConfig.api_key = '6PcspJiyEshZTzxZYgHZ'
-		data = quandl.get_table('WIKI/PRICES', ticker = ticker.upper(), paginate=True)
-		count = 0
-		total = len(data.index)
-		for i in range(3, len(data.index)):
-			AH, NH, AL, HL = calculateSupports(float(data.iloc[i - 3].high), float(data.iloc[i - 3].low), float(data.iloc[i - 2].high), float(data.iloc[i - 2].low), float(data.iloc[i - 2].close))
-			if (data.iloc[i - 1].close >= NH):
-				if upOrDip(data.iloc[i].open, data.iloc[i].close):
-					count+=1
-			elif (data.iloc[i - 1].close <= HL):
-				if not upOrDip(data.iloc[i].open, data.iloc[i].close):
-					count+=1
-			else:
-				count+=1
-
-		print("The correctness for this stock is", round((count / total) * 100, 2), "%")
-
-		AH, NH, AL, HL = calculateSupports(float(data.iloc[-2].high), float(data.iloc[-2].low), float(data.iloc[-1].high), float(data.iloc[-1].low), float(data.iloc[-1].close))
-		printSupports(AH, NH, AL, HL)
+	main()
 
